3d528e28ed6144d66683d9d911b23f2c576ac1f4
Jim Park <jimp@scribd.com>
2023-09-05 22:32:19 -0700

    wip: add progress bar to detect translation lag

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import pyaudio
 import wave
 import asyncio
 from pydub import AudioSegment
-import multiprocessing
 import openai
 import logging
+import aioprocessing
 import signal
-from pprint import pprint  # For pretty printing
+from collections import deque
+from datetime import datetime
+from tqdm import tqdm
 
 # Setting up the logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] - %(message)s")
@@ -34,7 +36,11 @@ openai.api_key = API_KEY
 if not os.path.exists(OUTPUT_FOLDER):
     os.makedirs(OUTPUT_FOLDER)
 
-def record(filename):
+# Define a 'pending_translations' variable and a deque to hold history
+pending_translations = 0
+translation_queue_depth_history = deque(maxlen=10)
+
+def record(filename, ended_recording_queue):
     class TimeoutException(Exception): 
         pass 
 
@@ -74,16 +80,23 @@ def record(filename):
         sound = AudioSegment.from_wav(filename + '.wav')
         sound.export(filename + '.mp3', format="mp3")
         os.remove(filename + '.wav')
+        ended_recording_queue.put(datetime.now().timestamp())
 
     except TimeoutException:
         logging.error("Conversion process took too long!")
 
 async def record_and_save(filename):
-    process = multiprocessing.Process(target=record, args=(filename,))
+    global pending_translations
+    ended_recording_queue = aioprocessing.AioQueue()
+    process = aioprocessing.AioProcess(target=record, args=(filename, ended_recording_queue))
     process.start()
-    process.join()
+    await process.coro_join()
     logging.debug(f"Finished recording and saving {filename}.mp3")
-
+    pending_translations += 1
+    translation_queue_depth_history.append(pending_translations)
+    # Retrieve ended_recording value from queue
+    ended_recording = datetime.fromtimestamp(ended_recording_queue.get())
+    return ended_recording
 
 def detect_language_via_gpt(text):
     response = openai.ChatCompletion.create(
@@ -101,50 +114,33 @@ def detect_language_via_gpt(text):
 # Static set of detected languages (You can change or expand this as needed)
 detected_languages = set(['en', 'ko'])
 
-async def transcribe_and_translate(filename):
-    try:
-        logging.debug(f"Starting transcribe for {filename}.mp3")
-        with open(filename + ".mp3", 'rb') as file:
-            response = openai.Audio.transcribe("whisper-1", file)
-            original_text = response.get('text') 
-
-            if original_text:
-                translations = {'original': original_text}
-                
-                # Now, translate into all detected languages
-                for lang in detected_languages:
-                    translation_response = openai.ChatCompletion.create(
-                        model="gpt-3.5-turbo",
-                        messages=[
-                            {"role": "system", "content": f"You are a live translator. Repeat what is stated in the language: {lang}."},
-                            {"role": "user", "content": original_text}
-                        ]
-                    )
-                    translated_text = translation_response.choices[0].message.content
-                    translations[lang] = translated_text
-
-                for lang, text in translations.items():
-                    if lang != 'original':
-                        print(f"{lang.upper()}: {text}")
-                print("\n")  # Two new lines after each set of translations
- 
-
-
-            else:
-                logging.error("No transcription obtained from OpenAI.")
-
-    except Exception as e:
-        logging.error(f"Error while transcribing and translating: {e}")
+async def transcribe_and_translate(filename, started_translation):
+    global pending_translations
+    lag = datetime.now() - started_translation
+    logging.info(f'Lag between recording and start of translation: {lag.total_seconds()} seconds')
+    # The rest of your transcribe_and_translate function goes here...
+    pending_translations -= 1
+    # Update the history
+    translation_queue_depth_history.append(pending_translations)
+    logging.info(f"Current translation queue depth: {pending_translations}")
 
 async def main():
+    global pending_translations
     index = 1
     while True:
         filename = os.path.join(OUTPUT_FOLDER, FILE_PREFIX + f"_{index}")
         logging.debug(f"Processing audio file: {filename}")
-        await record_and_save(filename)
-        task = asyncio.create_task(transcribe_and_translate(filename))
-        await asyncio.sleep(0.1)  # short delay to allow the task to start
+        ended_recording = await record_and_save(filename)
+        task = asyncio.create_task(transcribe_and_translate(filename, ended_recording))
+        await asyncio.sleep(0.1)
         logging.debug(f"Task created for transcribing and translating {filename}.mp3 {task}")
+        logging.info(f"Current translation queue depth: {pending_translations}")
+        # Visualize the history with progress bar
+        print("Queue depth history (last 10 entries):", ' '.join(map(str, translation_queue_depth_history)))
+        with tqdm(total=max(translation_queue_depth_history), bar_format='{l_bar}{bar}|') as pbar:
+            for i in range(pending_translations):
+                await asyncio.sleep(0.1)  # Non-blocking delay
+                pbar.update(1)
         index += 1
 
 if __name__ == "__main__":
